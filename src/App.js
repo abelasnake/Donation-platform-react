9d2c4e271848d3dd9c867f112bd73a11ec294925
hedishafi <hafsahussein1221@gmail.com>
2024-03-21 08:33:30 +0300

    other changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import Programs from "./components/Programs";
+import Title from "./components/Title";
+import About from "./components/About";
+import Organization from "./components/Orgnization";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Dashboard from "./admin/Dashboard";
+import MoneyDonation from "./admin/Money";
+import GoodsDonation from "./admin/Goods";
+import Volunteer from "./admin/Volunteer";
+import Layout from "./components/Layout";
+import HomeNews from "./components/HomeNews";
+import DonationForm from "./components/DonationForm";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+
+const HomeStuff = () => {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+      <Title subTitle="Our Programs" title="What we offer" />
+      <Programs />
+      <About />
+      <Title subTitle="Gallery" title="Eznet Photos" />
+      <Organization />
+      <Title subTitle="Contact Us" title="Get in Touch" />
+      <Contact />
+      <Title subTitle="Our News" title="Latest News" />
+      <HomeNews />
+      <Footer />
+
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeStuff />} />
+        <Route
+          path="/admin/*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/money_donation" element={<MoneyDonation />} />
+                <Route path="/goods_donation" element={<GoodsDonation />} />
+                <Route path="/volunteer_donation" element={<Volunteer />} />
+              </Routes>
+            </Layout>
+          }
+        />
+        <Route path="/donation_form" element={<DonationForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
